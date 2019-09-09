1726e39e938ecc024e57da45aadefc4d8bb49370
shella <dimasshella04@gmail.com>
2019-09-09 15:07:57 +0700

    coba commit

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/SimpleActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/SimpleActivity.java
@@ -25,6 +25,6 @@ public class SimpleActivity extends AppCompatActivity {
 
     public void handleSubmit(View view) {
         String name = nameInput.getText().toString();
-        outputText.setText("Hello " + name);
+        outputText.setText("Hai " + name);
     }
 }
